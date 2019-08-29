667fb3c9e8ac91dd2aa9e52f8b52b78b25850508
JA@02697400 <jahlil.allen@bison.howard.edu>
2019-08-29 19:32:39 -0400

    added functions to marginProblem.py

--- marginProblem.py
+++ marginProblem.py
@@ -1,2 +1,12 @@
-def marginProblem:
+def marginProblem():
+    #main function
+
+def lineCharacterCount(line):
+    #Counts the characters that can fit on one line
+
+def movesLeftMargin(startNum):
+    #starts cursor at "" inch(es) on left Margin
+
+def movesRightMargin(EndNum):
+    #ends cursor at "" inch(es) on Right Margin
     
