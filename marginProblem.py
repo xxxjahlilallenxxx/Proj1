5126a60c30499454e735ca573173d5aafa820c5a
Jah'lil Allen <jahlil.allen@bison.howard.edu>
2019-08-30 00:11:03 +0000

    added input variables

--- marginProblem.py
+++ marginProblem.py
@@ -1,5 +1,7 @@
 def marginProblem():
     #main function
+    leftM = input("Please enter a number for left margin: ")
+    rightM = input("Please enter a number for right margin: ")
 
 def lineCharacterCount(line):
     #Counts the characters that can fit on one line
