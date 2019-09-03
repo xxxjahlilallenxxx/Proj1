305069f1da76b9b280cac80949645ebefac0c6a1
JA@02697400 <jahlil.allen@bison.howard.edu>
2019-09-02 21:11:18 -0400

    putting all files back

--- marginProblem.py
+++ marginProblem.py
@@ -1,14 +1,24 @@
-def marginProblem():
-    #main function
-    leftM = input("Please enter a number for left margin: ")
-    rightM = input("Please enter a number for right margin: ")
-
 def lineCharacterCount(line):
     #Counts the characters that can fit on one line
 
 def movesLeftMargin(startNum):
     #starts cursor at "" inch(es) on left Margin
+    formatter.push_margin(startNum)
+    
+def marginProblem():
+    #main function
+    leftM = input("Please enter a number for left margin: ")
+    rightM = input("Please enter a number for right margin: ")
+    movesLeftMargin(leftM)
+    finput = open('testFile.txt','r')
+    finputList = finput.read()
+    while len(finputList) > 0:
+        if (len(finputList)) > 80:
+            finputList
+        
+    
 
-def movesRightMargin(EndNum):
-    #ends cursor at "" inch(es) on Right Margin
+
+
+
     
