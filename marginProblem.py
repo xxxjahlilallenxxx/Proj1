5f4c14bb12bd6ee7d6fe6fbd2325ab05459dcf88
Jah'lil Allen <jahlil.allen@bison.howard.edu>
2019-09-03 13:25:34 +0000

    Somewhat done just going to test

--- marginProblem.py
+++ marginProblem.py
@@ -1,17 +1,23 @@
-def marginProblem():
-    #main function
-    leftM = input("Please enter a number for left margin: ")
-    finput = open('testFile.txt', 'r')
-    movesLeftMargin(leftM)
-    finputList = finput.read()
-    
-    
-    
-
 def lineCharacterCount(line):
     #Counts the characters that can fit on one line
 
 def movesLeftMargin(startNum):
-    #starts cursor at <number> inch(es) on left Margin
+    #starts cursor at "" inch(es) on left Margin
     formatter.push_margin(startNum)
+    
+def marginProblem():
+    #main function
+    leftM = input("Please enter a number for left margin: ")
+    rightM = input("Please enter a number for right margin: ")
+    movesLeftMargin(leftM)
+    finput = open('testFile.txt','r')
+    finputList = finput.read()
+    textFile = open('DAT1.txt','w')
+    while len(finputList) > 0:
+        if (len(finputList)) > 80:
+            textFile.write(finputList[0:80])
+            for x in range(0,80):
+              finputList.pop()
+        else:
+            textFile.write(finputList)
     
